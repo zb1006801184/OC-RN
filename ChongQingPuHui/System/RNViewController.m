ee83259869fc4d42ff3ef6f0d11276470b6d0e09
zb1006801184 <www.1006801184@qq.com>
2017-11-15 17:44:30 +0800

    11

--- ChongQingPuHui/System/RNViewController.m
+++ ChongQingPuHui/System/RNViewController.m
@@ -23,8 +23,6 @@ RCT_EXPORT_METHOD(RNViewControllerClick:(NSString *)msg){
     NSLog(@"RN传入原生界面的数据为:%@",msg);
     //主要这里必须使用主线程发送,不然有可能失效
     dispatch_async(dispatch_get_main_queue(), ^{
-        MainViewController *one = [[MainViewController alloc]init];
-        
         AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication] delegate];
         [app.nav popViewControllerAnimated:YES];//返回到上一层VC
         //跳转到VC push
